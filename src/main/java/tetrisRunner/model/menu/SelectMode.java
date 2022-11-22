b74516f5bf84dee4f038cdaa4c7d7d5647b76ca7
João Longras <joaolongras2003@gmail.com>
2022-11-22 19:33:38 +0000

    Added menu GameMode

---
+++ src/main/java/tetrisRunner/model/menu/SelectMode.java
@@ -0,0 +1,26 @@
+package tetrisRunner.model.menu;
+
+import java.util.Arrays;
+
+public class SelectMode extends Menu{
+    public SelectMode() {
+        super.entries = Arrays.asList("Classic", "Co-op", "1v1", "Return");
+    }
+
+    public boolean isSelectedClassic() {
+        return isSelected(0);
+    }
+
+    public boolean isSelectedCo_Op() {
+        return isSelected(1);
+    }
+
+    public boolean isSelected1v1() {
+        return isSelected(2);
+    }
+
+    public boolean isSelectedReturn() {
+        return isSelected(3);
+    }
+}
+
