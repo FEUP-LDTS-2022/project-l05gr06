d6c12ff87e91700a5ce7456c9b66920e4288f497
joaofeup <up202108829@up.pt>
2022-11-22 00:29:25 +0000

    Created states branch and added some lines

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,21 @@
+import gui.LanternaGUI;
+import states.MenuState;
+import states.State;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Game {
+
+    private final LanternaGUI gui;
+    private State state;
+
+    public Game() throws FontFormatException, IOException, URISyntaxException {
+        this.gui = new LanternaGUI(20, 20);
+        this.state = new MenuState(new Menu());
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world2!");
     }
--- src/main/java/states/State.java
+++ src/main/java/states/State.java
@@ -1,4 +1,30 @@
 package states;
 
-public abstract class State<T>{
+
+import controller.Controller;
+import gui.GUI;
+import viewer.Viewer;
+
+import java.io.IOException;
+
+public abstract class State<T> {
+    private final T model;
+    private final Controller<T> controller;
+    private final Viewer<T> viewer;
+
+    public State(T model) {
+        this.model = model;
+        this.viewer = getViewer();
+        this.controller = getController();
+    }
+
+    protected abstract Viewer<T> getViewer();
+
+    protected abstract Controller<T> getController();
+
+    public T getModel() {
+        return model;
+    }
+
+
 }
